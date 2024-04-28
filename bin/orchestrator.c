57e61be8e5f7101efba5e1a5857cdff35315b948
JohnnyLongo <longu@PCFIXOJOAO>
2024-04-28 17:34:15 +0100

    funcs de resposta a pedidos simples e status feitas, faço para pipelined depois

--- bin/orchestrator.c
+++ bin/orchestrator.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
+#include <time.h>
 
 void writeback(int pid){ //pipe entre servidor -> cliente
     char path[64];
@@ -22,6 +23,58 @@ int createFifo(char *path){
     }
     return(open(path, O_RDONLY | O_ASYNC));
 }
+
+
+//executa task (passar task.argument em lista de strings, id é o identificador da task)
+//A SER USADO APENAS POR UM PROCESSO FILHO DEDICADO A ESTA TASK (pq paralelismo)
+void execute_task(char *arg[], char *output_folder, int id){
+    
+    char end[100];                               //ficheiro destino
+    sprintf(end, "%s/%d", *output_folder, id);
+
+    int output = open(end, O_CREAT | O_WRONLY); //ficheiro destino aberto
+    int saved_stdout = dup(1);
+    dup2(output, 1);                            //execvp agora escreve para o ficheiro em vez de escrever para STD output
+    clock_t starttime = clock(), endtime;     //starttime guardado
+    execvp(arg[0], arg);                        //executado
+    endtime = clock() - starttime;              //endtime guardado
+    double time = (endtime * 1000) / CLOCKS_PER_SEC;        //time é os clock cycles a dividir pela frequencia do relogio (var global CLOCKS_PER_SEC do time.h)
+    char answer[100];
+    sprtinf(answer, "%d, '%s' time: %d", id, arg, time);
+    write(output, answer, strlen(answer));
+    write(output, itoa(time), sizeof(itoa(time)));
+    close(output);
+    dup2(saved_stdout, 1);                      //restaurar standard output (fanado do stack overflow)
+    close(saved_stdout);
+    _exit(0);
+}
+
+
+//It answer status. That's it, pretty self explanatory. answer_pipe is the answer pipe, queue is the queue, end_feito is where the ended things are feitas.
+void answer_status(int answer_pipe, QUEUE queue, char *end_feito){
+    char dir[strlen(end_feito)+2];
+    sprintf(dir, "%s/*", *end_feito);
+    write(answer_pipe, "ANSWERING:\n", sizeof("ANSWERING:\n"));
+    NODE *walker;
+    walker = queue.first;
+    char* answer[100];
+    write(answer_pipe, *answer, strlen(answer));
+    while(walker != queue.last){           //não sabia se a lista dava loop no fim então fiz assim
+        sprintf(answer, "%d: %s\n", walker->task.fd, walker->task.argument);
+        write(answer_pipe, answer, strlen(answer));
+        walker = walker->next;
+    }
+
+    write(answer_pipe, "\n\nDONE:\n", strlen("\n\nDONE:\n"));
+    int saved_output = dup(1);
+    dup2(answer_pipe, 1);
+    execlp("tail", "tail", "-n", "1", dir, NULL);   //bash para "dá-me a ultima linha destas coisas todas", que "convenientemente" está no formato perfeito *wink wink*
+    dup2(saved_output, 1);
+    close(saved_output);
+}
+
+
+
 int main(int argc, char **argv){
     QUEUE queue;        
     initQueue(&queue);
@@ -79,7 +132,7 @@ int main(int argc, char **argv){
             //int execvp(filelog,args);
     
         }
-
+        
         
     }
 }
