337494d6951ca644195c7f5272373751bb3d0af8
JohnnyLongo <73479827+JohnnyLongo@users.noreply.github.com>
2024-05-07 23:11:25 +0100

    Revert "mudei pasta e mais cenas no .h"
    This reverts commit a67dbe1cd4382d4f911e1a1fbe2b60c5e98334f3.

--- src/orchestrator.h
+++
@@ -1,7 +0,0 @@
-void writeback(int pid);
-int createFifo(char *path);
-void execute_task(char *arg[], char *output_folder, int id);
-void execute_singular(char *arg[], char *output_folder, int id);
-void execute_pipeline(char *arg[], char *output_folder, int id);
-void answer_status(int answer_pipe, QUEUE queue, char *end_feito);
-void doasIsay(int childno, char *logpath, int notifyServ, int getTask);
